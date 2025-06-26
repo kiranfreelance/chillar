451dc06eab351182793b8bda9f9f4a7a969c44b9
deepakAnkani <deepakdeepu2300@gmail.com>
2025-06-26 12:19:31 +0530

    Updates

--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export const formatINRCurrency = (amount: any) => {
-    return new Intl.NumberFormat('en-IN', {
-        style: 'currency',
-        currency: 'INR',
-        maximumFractionDigits: 2
-    }).format(amount);
-}
